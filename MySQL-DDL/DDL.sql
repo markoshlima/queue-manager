a6a2048a8394925654c426d02c6fc75801231170
Markos Lima <markoshlima@gmail.com>
2020-04-04 15:30:52 -0300

    DDL MySQL

--- MySQL-DDL/DDL.sql
+++ MySQL-DDL/DDL.sql
@@ -1,14 +1,12 @@
-CREATE DATABASE dbproject;
-
-CREATE TABLE `queue` (
-  `id_queue` varchar(40) CHARACTER SET utf8 NOT NULL,
-  `name` varchar(120) CHARACTER SET utf16 NOT NULL,
-  `text_queue` text CHARACTER SET utf8,
-  `end_queue` text,
-  `logo` varchar(50) DEFAULT NULL,
-  `date_register` datetime DEFAULT NULL,
-  `id_user` varchar(40) DEFAULT NULL,
-  PRIMARY KEY (`id_queue`)
+CREATE TABLE queue (
+  id_queue varchar(40) CHARACTER SET utf8 NOT NULL,
+  name varchar(120) CHARACTER SET utf16 NOT NULL,
+  text_queue text CHARACTER SET utf8,
+  end_queue text,
+  logo varchar(50) DEFAULT NULL,
+  date_register datetime DEFAULT NULL,
+  id_user varchar(40) DEFAULT NULL,
+  PRIMARY KEY (id_queue)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `queue_client` (
